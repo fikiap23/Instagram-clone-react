dc574575ab546b9445092b8011d87ebb0c1009db
fikiaprian23 <fikiaprian23@gmail.com>
2023-09-10 14:26:04 +0700

    [ADD] Profile user details

---
+++ src/Pages/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,13 @@
+import { ProfileUserDetails } from '../../Components/Profile/ProfileUserDetails'
+
+const ProfilePage = () => {
+  return (
+    <div className="px-20">
+      <div>
+        <ProfileUserDetails />
+      </div>
+    </div>
+  )
+}
+
+export default ProfilePage
